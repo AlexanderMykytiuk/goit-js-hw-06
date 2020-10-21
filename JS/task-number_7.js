a4d5797ec7099cc5a0d119f4389ffb613b78f41b
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-10-21 12:58:29 +0300

    Задача №7

---
+++ JS/task-number_7.js
@@ -0,0 +1,8 @@
+// Write code under this line
+const bal = {balance : 0}
+function ecvBal  ({balance})  {return balance;}
+ const calculateTotalBalance = users => users.reduce((acc, user) => acc + user.balance, 0);
+ 
+console.log(calculateTotalBalance(users)); 
+// 20916
+
