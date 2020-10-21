0a6d40cafa839e918a6db6b56f6bc50607b10968
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-10-21 14:16:22 +0300

    Завершив модуль 6

---
+++ JS/task-number_10.js
@@ -0,0 +1,22 @@
+// Write code under this line
+const getSortedUniqueSkills = array => {
+    
+    let skillsNotUnique = array
+    .reduce((acc, {skills}) => [...acc, ...skills], []);
+
+    skillsNotUnique = skillsNotUnique.sort();
+
+    return [...new Arr(skillsNotUnique)];
+};
+
+console.log(getSortedUniqueSkills(users));
+/* [ 'adipisicing', 'amet',
+ 'anim', 'commodo',
+ 'culpa', 'elit',
+ 'ex', 'ipsum',
+ 'irure', 'laborum',
+ 'lorem', 'mollit',
+ 'non', 'nostrud',
+ 'nulla', 'proident',
+ 'tempor', 'velit',
+ 'veniam' ]; */
